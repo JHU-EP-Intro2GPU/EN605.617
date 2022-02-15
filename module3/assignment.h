d0499583448c34702a46074e05b3d6dd489aac8c
Kevin Phillips <kevin.phillips@jhuapl.edu>
2022-02-14 21:43:06 -0500

    Finished

--- module3/assignment.h
+++ module3/assignment.h
@@ -1,5 +1,62 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
+
+////////////////////////////////////////////////////////////////////////////////
+// Cuda kernels
+
+// Add two 1-d arrays of unsigned ints together 
+__global__
+static void add_matrices(unsigned int dest_matrix[],
+	const unsigned int matrix_i[], const unsigned int matrix_j[])
+{
+    const unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	// dest_matrix[i] = threadIdx.x + matrix_j[i];
+    dest_matrix[i] = matrix_i[i] + matrix_j[i];
+}
+
+// Subtract two 1-d arrays of unsigned ints together 
+__global__
+static void subtract_matrices(unsigned int dest_matrix[],
+	const unsigned int matrix_i[], const unsigned int matrix_j[])
+{
+    const unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    dest_matrix[i] = matrix_i[i] - matrix_j[i];
+}
+
+// Multiply two 1-d arrays of unsigned ints together 
+__global__
+static void multiply_matrices(unsigned int dest_matrix[],
+	const unsigned int matrix_i[], const unsigned int matrix_j[])
+{
+    const unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    dest_matrix[i] = matrix_i[i] * matrix_j[i];
+}
+
+// Modulo divide two 1-d arrays of unsigned ints together 
+__global__
+static void modulo_matrices(unsigned int dest_matrix[],
+	const unsigned int matrix_i[], const unsigned int matrix_j[])
+{
+    const unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    dest_matrix[i] = matrix_i[i] % matrix_j[i];
+}
+
+// Modulo divide two 1-d arrays of unsigned ints together 
+__global__
+static void modulo_matrices_branched(unsigned int dest_matrix[],
+	const unsigned int matrix_i[], const unsigned int matrix_j[])
+{
+    const unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    // Replace any invalid modulo divisors with 0
+    dest_matrix[i] = (matrix_j[i] > 0) ? matrix_i[i] % matrix_j[i] : 0;
+}
+////////////////////////////////////////////////////////////////////////////////
 
 template<typename T>
-inline void print_vector (const std::vector<T> &);
+static inline void print_vector(const std::vector<T> & vec) {
+	std::cout << "[";
+    for (auto i : vec) {
+        std::cout << i << ",";
+    }
+	std::cout << "]\n";
+}
--- module3/run.sh
+++ module3/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-./assignment.exe 64 64
+if [ $# == 2 ]
+then
+    ./assignment.exe $1 $2
+    ./assignment_advanced.exe $1 $2
+else
+    echo $1
+    echo $2
+    echo "Usage: $0 [block size] [number of threads per block]"
+fi
